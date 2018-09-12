b3b39fcd9a2881f2c9b7a9abdb79909d16d28c71
Ahmed <ahmedbendaya@outlook.com>
2018-09-12 18:34:44 -0400

    Adding some styles and organizing css

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@ var gulp = require('gulp'),
     watch = require("gulp-watch"),
     postcss = require('gulp-postcss'),
     autoprefixer = require('autoprefixer'),
-    cssvars = require('postcss-simple-vars');
+    cssvars = require('postcss-simple-vars'),
+    nested = require('postcss-nested'),
+    cssImport = require('postcss-import');
 
 
 gulp.task('default', function () {
@@ -15,7 +17,7 @@ gulp.task('html', function () {
 
 gulp.task('styles', function () {
     return gulp.src('./app/assets/styles/styles.css')
-        .pipe(postcss([ cssvars,autoprefixer]))
+        .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
         .pipe(gulp.dest('./app/temp/styles'));
 
 });
@@ -27,5 +29,5 @@ gulp.task('watch', function () {
 
     watch('./app/assets/styles/**/*.css', function () {
         gulp.start('styles');
-    })
+    });
 });
